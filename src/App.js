a04c89d9166ee9991819af4f6d3d9a39a77e81db
allenLQVE <allenlqve@gmail.com>
2024-08-15 15:40:36 -0400

    frontend initial & being able to get data from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import './App.css';
 
+import { Table } from 'reactstrap'
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/records/').then(
+            response => {
+                setData(response.data);
+            }
+        ).catch(error => {
+            console.error(error);
+        })
+    }, [])
+
+    return (
+        <div className='records'>
+            <h1>Records</h1>
+            <Table bordered striped size="sm">
+                <thead>
+                    <tr>
+                        <th>Item Name</th>
+                        <th>Store Name</th>
+                        <th>Purchase Date</th>
+                        <th>Cost</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data?.map((data) => (
+                        <tr>
+                            <td>{data.item.name}</td>
+                            <td>{data.store.name}</td>
+                            <td>{data.purchaseDate}</td>
+                            <td>${data.price}</td>
+                            {/* add a col for removing or update record */}
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </div>
+    );
 }
 
 export default App;
